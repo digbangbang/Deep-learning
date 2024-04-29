f41468339e298af6caee229093184fd518cb0ced
ZhiWei Li <78746384+digbangbang@users.noreply.github.com>
2024-04-29 18:31:09 +0800

    Add files via upload

---
+++ data_loader.py
@@ -0,0 +1,17 @@
+import numpy as np
+import os
+import struct
+def load_mnist(path, kind='train'):
+    """加载MNIST数据从`path`"""
+    labels_path = os.path.join(path, f'{kind}-labels-idx1-ubyte')
+    images_path = os.path.join(path, f'{kind}-images-idx3-ubyte')
+
+    with open(labels_path, 'rb') as lbpath:
+        magic, n = struct.unpack('>II', lbpath.read(8))
+        labels = np.fromfile(lbpath, dtype=np.uint8)
+
+    with open(images_path, 'rb') as imgpath:
+        magic, num, rows, cols = struct.unpack(">IIII", imgpath.read(16))
+        images = np.fromfile(imgpath, dtype=np.uint8).reshape(len(labels), 784)
+
+    return images, labels
